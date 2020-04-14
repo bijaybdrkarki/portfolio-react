0c05974bf1531fc758c281fd0a4973923804bc74
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-14 01:35:47 -0400

    navbar jsx and css created

---
+++ portfolio/src/components/navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from '../img-video/logo.png';
+import {Link} from 'react-router-dom';
+import './navbar.css';
+
+const Navbar = () => {
+
+    return (    
+        <div className="navbar">
+            <div className="your-text">
+                <div className="logo">
+                    <Link to='/'><img src={logo} alt="logo" /></Link>
+                </div>
+                <div className="menu">
+                    <ul>
+                        <li><Link to='#'> CODE STATION </Link></li>
+                        <li><Link to='/about'>ABOUT</Link></li>
+                        <li><Link to='#'>PROJECTS</Link></li>
+                        <li><Link to='#'>CONNECT</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
