dfb375b659549a2262434237ebd6a875f1e6d8be
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-14 01:37:56 -0400

    created home component

---
+++ portfolio/src/components/home/home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from '../navbar';
+import video from '../../img-video/Source-Code.mp4'
+import "./home.css"
+
+const Home = () => {
+
+return (
+    <header>
+        <Navbar />
+        <video autoPlay muted loop id="myVideo">
+            <source src={video} type="video/mp4" />
+                Your browser does not support HTML5 video.
+        </video>
+
+        <section className="videoSection">
+            <h1>WEB DESIGN &</h1>
+            <h1>DEVELOPMENT ?</h1>
+        </section>
+
+        
+    </header>
+)
+
+}
+
+export default Home;
