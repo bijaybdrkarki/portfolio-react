b4e12aa1bf35bd7792387134b4780cda6e56eaaa
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-14 01:36:48 -0400

    about component created with slick slide carousel

---
+++ portfolio/src/components/about/about.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Navbar from '../navbar';
+import "./about.css"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+// import "slick-carousel/slick/slick-theme.css";
+import './slick-theme.css'
+//used own customized slick-theme.css instead of css given by react-slick
+
+const About= ()=> {
+    const settings = {
+        dots: true,
+        arrows : false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000 
+      };
+    return (
+        <header>
+             <Navbar />
+            <Slider {...settings}> 
+                <div className="slide1"></div>
+                <div className="slide2"></div>
+                <div className="slide3"></div>
+            </Slider>           
+           
+        </header>          
+    )
+
+}
+
+export default About;
