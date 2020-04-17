d88751219ef0826182bc024d0cb40432176e542f
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-17 17:58:18 -0400

    added new section with link to resume

--- portfolio/src/components/about/about.js
+++ portfolio/src/components/about/about.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import Navbar from '../navbar/navbar';
 import Footer from '../footer/footer'
 import styles from "./about.module.css"
@@ -74,10 +75,16 @@ const About= ()=> {
                 <h1>About Myself</h1>
                 <p>
                     These are something that resume can not explain. I love to drink Tea/coffee alot.
-                    My favorite number is 2 because 1/top is easy to get and very hard to maintain. Also 2nd makes you work hard.
-                    I love watching football (soccer) sepcially premier league. And I love to play FIFA.
+                    My favorite number is 2 because 1/top is easy to get and very hard to maintain. Also 2nd makes you work hard. I enjoy watching football (soccer) sepcially premier league <i class="far fa-futbol"></i>. And I love to play FIFA. Lastly, i love to discuss in person and solve the problem.
                 </p> 
             </div>       
+        </section>
+        <section className={styles.strength}>
+            <h1> Know My Strength & How I can assists you </h1>
+            <p>How about looking more into what i have done in past few years and think how working with me be fruitfull to you. My resume is standarized to this particular area but please remember that resume doesnot explain everything about that person.</p>
+            <div className={styles.seeResume}>
+                <Link to="/resume">View Resume <i class="fas fa-arrow-right"></i></Link>
+            </div> 
         </section>
         <Footer /> 
         </>       
