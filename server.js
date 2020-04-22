1059e2acdd52c12e786c98e5e9a8ffcdb3d3cfc9
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-21 20:17:44 -0400

    added code to serve react app as static file

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'portfolio/build')))
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/portfolio/build/index.html'))
+})
+}
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
@@ -15,5 +26,7 @@ app.post('/api/world', (req, res) => {
   // console.log(req.body.p);
   res.send(`I received your POST request. This is what you sent me: ${req.body.p}`);
 });
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
