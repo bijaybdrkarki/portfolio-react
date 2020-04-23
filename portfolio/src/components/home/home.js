8feac8b5ab8a988c9fab2894198f75d57ba21712
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-23 18:23:21 -0400

    added new section

--- portfolio/src/components/home/home.js
+++ portfolio/src/components/home/home.js
@@ -22,9 +22,18 @@ return (
             <Link to="/connect" className={styles.connect}>LET'S CONNECT</Link> 
         </section> 
     </header>
-    <main>
-    <p>this main body</p>
-    </main>
+    <section className={styles.homeSection}>
+        <img src={require('../../img-video/home.png')} alt="nepal" className={styles.homeImg}/>
+        <div className={styles.homeText}>
+            <h1>About Myself</h1>
+            <p>
+                These are something that resume can not explain. I love to drink Tea/coffee alot.
+                My favorite number is 2 because 1/top is easy to get and very hard to maintain. Also 2nd makes you work hard. I enjoy watching football (soccer) sepcially premier league <i className="far fa-futbol"></i>. And I love to play FIFA. Lastly, i love to discuss in person and solve the problem.
+            </p> 
+        </div>       
+    </section>
+        
+    
     
     <Footer />
     </>
