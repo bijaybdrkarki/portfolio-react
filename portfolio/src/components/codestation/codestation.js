d1921fca8c07c6ca3950d41023945a2e27e62121
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-22 20:06:14 -0400

    added useEffect for component unmount

--- portfolio/src/components/codestation/codestation.js
+++ portfolio/src/components/codestation/codestation.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {Link} from 'react-router-dom';
 import Navbar from '../navbar/navbar';
 import Footer from '../footer/footer'
 import styles from "./codestation.module.css"
 
 const Codestation = () =>{
+
+    useEffect(()=>{
+        return () => {
+            
+        }
+    },[])
 
     return (
         <>
@@ -14,7 +21,19 @@ const Codestation = () =>{
                 <h1>WEB DESIGN &</h1>
                 <h1>CODESTATION.</h1>
             </section>         
-        </header> 
+        </header>    
+        <section className={styles.allproducts}>
+            <h1>All Products</h1>
+            <div className={styles.product}>
+                <div className={styles.shopify}>
+                    <img src={require('../../img-video/shopify.png')} alt="shopify" />
+                    <div className={styles.shopifycenter}>
+                        <p>Shopify side card</p>
+                        <Link to='/codestation/sidecart' className={styles.projectlink}>GET IT</Link>
+                    </div>
+                </div>
+            </div>
+        </section>
         <Footer /> 
         </>       
 )}
