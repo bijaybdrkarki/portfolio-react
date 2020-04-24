4bacb1244f7de4bc8dc50b05cea3f1d8f52ee94c
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-23 22:06:54 -0400

    edited homesection and added aboutcode section, recent projects section

--- portfolio/src/components/home/home.js
+++ portfolio/src/components/home/home.js
@@ -25,16 +25,50 @@ return (
     <section className={styles.homeSection}>
         <img src={require('../../img-video/home.png')} alt="nepal" className={styles.homeImg}/>
         <div className={styles.homeText}>
-            <h1>About Myself</h1>
+            <h1>Hi, I am Bijay</h1>
             <p>
-                These are something that resume can not explain. I love to drink Tea/coffee alot.
-                My favorite number is 2 because 1/top is easy to get and very hard to maintain. Also 2nd makes you work hard. I enjoy watching football (soccer) sepcially premier league <i className="far fa-futbol"></i>. And I love to play FIFA. Lastly, i love to discuss in person and solve the problem.
+                Have you ever imagined of having a website for your business that clearly shows what you do in your business or personal portfolio that explain who you are and you can do ?
+            <br /><br /> Let's connect, give me a call (or email) and build a website for you.
             </p> 
-        </div>       
+        </div>      
     </section>
-        
-    
-    
+    <section className={styles.aboutCode}>
+        <h1>I also provide code snippets for Shopify, React and Express.js</h1>
+        <div className={styles.codetype}>
+            <article>
+                <h2>Customize your shopify store</h2>
+                <p>Do you want to customize you shopify store, add some extra features that makes you unique from other and help your customer feel easy on your store ?</p>
+            </article>
+            <article>
+                <h2>Customize your React frontend</h2>
+                <p>Some components you would like to add to your React App</p>
+            </article>
+            <article>
+                <h2>Need help on Express server</h2>
+                <p>combine your frontend with express server, add Mongodb database, send sms/email to your user</p>
+            </article>
+        </div>
+        <Link to="/codestation" className={styles.codestation}>CODE STATION <i className="fas fa-arrow-right"></i></Link> 
+    </section>
+    <section className={styles.recentproject}>
+        <h1>Most Recent Projects</h1>
+        <div className={styles.develop}>
+            <div className={styles.airbnb}>
+                <img src={require('../../img-video/airbnb.png')} alt="airbnb" />
+                <div className={styles.airbnbcenter}>
+                    <p>Node.js MVC project</p>
+                    <Link to="//web120.herokuapp.com/" target="_blank" className={styles.connect}>Airbnb project</Link>
+                </div>
+            </div>
+            <div className={styles.shopify}>
+                <img src={require('../../img-video/shopify.png')} alt="shopify" />
+                <div className={styles.shopifycenter}>
+                    <p>Shopify Liquid project</p>
+                    <Link to="//jhupro.myshopify.com/" target="_blank" className={styles.connect}>Shopify project</Link>
+                </div>
+            </div>
+        </div>
+    </section>
     <Footer />
     </>
 )}
